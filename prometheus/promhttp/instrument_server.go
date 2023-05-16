5dc6c9498d775857ab9893d27d5b55e6f0bed077
Johnny <johnny@oligosecurity.io>
2023-05-16 10:58:23 +0000

    Bugfix for data race in instrument_server.go

--- prometheus/promhttp/instrument_server.go
+++ prometheus/promhttp/instrument_server.go
@@ -389,14 +389,7 @@ func isLabelCurried(c prometheus.Collector, label string) bool {
 	return true
 }
 
-// emptyLabels is a one-time allocation for non-partitioned metrics to avoid
-// unnecessary allocations on each request.
-var emptyLabels = prometheus.Labels{}
-
 func labels(code, method bool, reqMethod string, status int, extraMethods ...string) prometheus.Labels {
-	if !(code || method) {
-		return emptyLabels
-	}
 	labels := prometheus.Labels{}
 
 	if code {
